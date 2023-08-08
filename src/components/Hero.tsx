2efe334b0edcf0201fd9624bd76292dae1b06e1a
João Pedro Cabral dos Santos <c4joaoc4@hotmail.com>
2023-08-08 13:09:34 -0300

    next font adjustment

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>
+            <h1>Teste</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
